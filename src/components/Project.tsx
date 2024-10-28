7520118243f30439058bfef31dd806ef6fe4c365
Evgeni Nikolov <evgeni.nikolov9009@gmail.com>
2024-10-28 23:45:54 +0000

    Project Section layoult complete. Project card complete. - Note Uses boilerplate text

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,22 +1,39 @@
-import React from "react";
+import { ReactElement } from "react";
+import styles from "./comp_css/components.module.css";
+import { BsBoxArrowUpRight } from "react-icons/bs";
 
 const Project = ({
 	title,
+	techStack,
 	description,
 	projectLink,
 	linkText,
+	projectImg,
 }: {
 	title: string;
+	techStack: string;
 	description: string;
 	projectLink: string;
 	linkText: string;
+	projectImg: ReactElement;
 }) => {
 	return (
 		<>
-			<div className="project-wrapper">
-				<h3>{title}</h3>
-				<p>{description}</p>
-				<a href={projectLink}>{linkText}</a>
+			<div className={styles.projectWrapper}>
+				<div className={styles.projectImgWrapper}>{projectImg}</div>
+				{/* {projectImg} */}
+
+				<div className={styles.projectInfo}>
+					<h3>{title}</h3>
+					<h4>
+						Technologies used: <span>{techStack}</span>
+					</h4>
+					<p>{description}</p>
+					<a href={projectLink} target="_blank">
+						{linkText}
+						<BsBoxArrowUpRight />
+					</a>
+				</div>
 			</div>
 		</>
 	);
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+// import React from "react";
 import { FaDownload } from "react-icons/fa";
 import "./comp_css/projects.css";
 import styles from "./comp_css/components.module.css";
 import Project from "./Project";
+import portfolioLanding from "../assets/portfolioLanding.png";
+import greenandspotHeader from "../assets/greenandspot_header.png";
 
 function Projects() {
 	return (
@@ -17,9 +19,29 @@ function Projects() {
 				<div className="projects-wrapper">
 					<Project
 						title="Green & Spot Joinery"
+						techStack="React.js JavaScript Vite"
 						description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure velit illo repellat? Repellendus nam vel fugiat quae voluptas similique aspernatur."
+						projectLink="https://greenandspot.co.uk/"
+						linkText="Live "
+						projectImg={
+							<img
+								src={greenandspotHeader}
+								alt="Laptop with carpentry website on the screen"
+							/>
+						}
+					/>
+					<Project
+						title="My Personal Portfolio"
+						techStack="React.js TypeScript Vite Git"
+						description="Lorem ipsum doloasdjalksbfjhlsbafhjlsdbfhljabfalhjbdfr sit, amet consectetur adipisicing elit. Iure velit illo repellat? Repellendus nam vel fugiat quae voluptas similique aspernatur."
 						projectLink="/"
-						linkText="Live Website"
+						linkText="Github "
+						projectImg={
+							<img
+								src={portfolioLanding}
+								alt="Laptop with carpentry website on the screen"
+							/>
+						}
 					/>
 				</div>
 			</div>
